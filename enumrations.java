002f4ec1fea88f0d3c9536a987112d64e3f52e29
Ninja-core <jd9087222@gmail.com>
2026-02-23 15:53:47 +0530

    enumerations new changes

--- enumrations.java
+++ enumrations.java
@@ -1,8 +1,15 @@
 public class enumrations {
+
     public static void main(String args[]){
-        enum names{kulwinder, mehr,rahul,saima}
-        names n = names.saima;
-        System.out.println(n);
+        // enum names{kulwinder, mehr,rahul,saima}
+        // names n = names.saima;
+        // System.out.println(n);
+        void sum(int a , double b, int... nums){
+            int s = Arrays.stream(nums).sum();
+            for(int v:nums){
+                s+=v;
+            }
+        }
     }
 }
 
