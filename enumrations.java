19af7fb41105370d119dc0647829853b7ebbe04d
Ninja-core <jd9087222@gmail.com>
2026-02-23 15:28:58 +0530

    enumerations

---
+++ enumrations.java
@@ -0,0 +1,11 @@
+public class enumrations {
+    public static void main(String args[]){
+        enum names{kulwinder, mehr,rahul,saima}
+        names n = names.saima;
+        System.out.println(n);
+    }
+}
+
+
+
+//  enumeratiosn is special type of data type where we define a fixed set of constants
