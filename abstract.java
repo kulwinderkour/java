9737d6deb7b27eb95b6d7489c955223f44d9632d
Ninja-core <jd9087222@gmail.com>
2026-02-27 10:24:26 +0530

    abstract classes added

--- abstract.java
+++ abstract.java
@@ -1,9 +1,11 @@
 // abstract class is a class that is declared with the abstract ekyword , abstract classes are not interfaces
 // it can hvae constructors
 // abstract method is declared and does not have implementation is known as an abstract class (abstract class may or may not have have an abstract method)
-// and classes which have absteact method 
+// and classes which have absteact method  adn we have contructros in abstract classes
 // advantages:
 // reduce the complexity, increase reusability 
 // abstract class dont have objects
-// 
 
+// properties of intrefaces: 
+// no constructors and interface cannot have object variable
+//
