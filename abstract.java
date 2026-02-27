221a311097eed81bcc181db640d31b529733563a
Ninja-core <jd9087222@gmail.com>
2026-02-27 10:09:13 +0530

    abstract classes added

--- abstract.java
+++ abstract.java
@@ -1,3 +1,6 @@
-// abstract class is a class that is declared with the abstract ekyword
+// abstract class is a class that is declared with the abstract ekyword , abstract classes are not interfaces
 // it can hvae constructors
 // abstract method is declared and does not have implementation is known as an abstract class
+// advantages:
+// reduce the complexity, increase reusability 
+
